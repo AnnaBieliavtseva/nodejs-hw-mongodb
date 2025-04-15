93bd1ffdadd026f80cfbb9427e30be6ca46addec
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2025-04-15 12:57:44 +0200

    fixed throwing errors

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -44,8 +44,7 @@ export const patchContactController = async (req, res, next) => {
   const result = await updateContact(id, req.body);
 
   if (!result) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
   res.json({
     status: 200,
@@ -58,8 +57,7 @@ export const deleteContactController = async (req, res, next) => {
   const { id } = req.params;
   const data = await deleteContact(id);
   if (!data) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
   res.status(204).send();
 };
