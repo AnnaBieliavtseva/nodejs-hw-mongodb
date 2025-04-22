2855b7f048bffcb5e5e2af5885a57e6aa8f70f68
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2025-04-22 21:07:45 +0200

    add parsedIsFavourite

--- src/utils/filters/parseContactFilterParams.js
+++ src/utils/filters/parseContactFilterParams.js
@@ -2,8 +2,10 @@ import { contactTypeList } from '../../constants/contacts.js';
 
 export const parseContactFilterParams = ({ type, isFavourite }) => {
   const parsedType = contactTypeList.includes(type) ? type : undefined;
+  const parsedIsFavourite =
+    isFavourite === "true" || isFavourite === "false" ? isFavourite : undefined;
   return {
     type: parsedType,
-    isFavourite,
+    isFavourite: parsedIsFavourite,
   };
 };
