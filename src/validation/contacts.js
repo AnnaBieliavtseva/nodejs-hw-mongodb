df8ccc2fae320198c7a2cbd551c9a266037c9fa7
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2025-04-24 08:58:01 +0200

    fix contactType validation

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -6,7 +6,7 @@ export const createContactSchema = Joi.object({
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid(...contactTypeList),
+  contactType: Joi.string().valid(...contactTypeList).required(),
 });
 
 export const updateContactSchema = Joi.object({
